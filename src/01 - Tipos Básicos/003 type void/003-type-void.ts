2909c8230289ed8963b5d22e6d2b894fea33b7dc
rubens-lavor <haryson07@gmail.com>
2021-04-03 23:10:23 -0300

    type void

--- src/01 - Tipos Básicos/003 type void/003-type-void.ts
+++ src/01 - Tipos Básicos/003 type void/003-type-void.ts
@@ -1,3 +1,15 @@
-function semRetorno(...args): void {
-  console.log()
+function semRetorno(...args: string[]): void {
+  console.log(args.join(' '))
 }
+const pessoa = {
+  nome: 'rubens',
+  sobrenome: 'lavor',
+
+  exibirNome(): void {
+    console.log(this.nome + ' ' + this.sobrenome)
+  },
+}
+semRetorno('rubens', 'lavor')
+pessoa.exibirNome()
+
+export { pessoa }
