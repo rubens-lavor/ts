80295ce0878d082cbb1861090ca43a69c62526a1
rubens-lavor <haryson07@gmail.com>
2021-04-09 20:54:53 -0300

    null and undefined

---
+++ src/01 - Tipos Básicos/07 - null - undefined/null-undefined.ts
@@ -0,0 +1,30 @@
+let x
+if (typeof x === 'undefined') x = 20
+console.log(x * 2)
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string
+  lastName?: string
+} {
+  return {
+    firstName,
+    lastName,
+  }
+}
+
+/* eslint-disable */
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x
+  return null
+}
+
+const squareOfTwoNumber = squareOf(2)
+
+if (squareOfTwoNumber === null) {
+  console.log('conta inválida')
+} else {
+  console.log(squareOfTwoNumber * 100)
+}
