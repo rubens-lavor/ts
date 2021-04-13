87317fc89e7a0268bffe6de8b9364980d4373625
rubens-lavor <haryson07@gmail.com>
2021-04-12 22:59:17 -0300

    union types

---
+++ src/01 - Tipos Básicos/11 - union types/union-types.ts
@@ -0,0 +1,8 @@
+function add(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b
+  return `S{a}${b}`
+}
+
+console.log(add(10, 20))
+console.log(add('10', '20'))
+console.log(add(10, '20'))
