0ad9ce19f7ed928e797b2beb40957daa3358734f
rubens-lavor <haryson07@gmail.com>
2021-04-14 22:39:00 -0300

    rename folder

---
+++ src/01 - Tipos Básicos/12 - tipos literais/tipos-literais.ts
@@ -0,0 +1,16 @@
+let x = 10 // eslint-disable-line
+x = 1010
+
+const y = 10
+const a = 100
+
+console.log(x, y, a)
+
+const pessoa = {
+  nome: 'josé' as const,
+  sobrenome: 'souza',
+}
+
+console.log(pessoa.nome)
+
+export default 1
