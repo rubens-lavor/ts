d0a4140ca08dae840bc5e2143019394438cd259c
rubens-lavor <haryson07@gmail.com>
2021-04-15 23:26:14 -0300

    Type assertion

---
+++ src/01 - Tipos Básicos/17 - type assertions/type-assertions.ts
@@ -0,0 +1,20 @@
+/* Recomendado */
+// condicional
+const body1 = document.querySelector('body')
+if (body1) body1.style.background = 'red'
+
+// Type assertion
+const body3 = document.querySelector('body') as HTMLBodyElement
+body3.style.background = 'red'
+
+//HTMLElement
+const input = document.querySelector('.input') as HTMLInputElement
+input.value = 'qualquer coisa'
+input.focus()
+
+/* Não recomendado */
+
+// Non-null assertion(!)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const body2 = document.querySelector('body')!
+body2.style.background = 'red'
