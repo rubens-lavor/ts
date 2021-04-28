f2d30d0a168ae5b1273bfe0682b75bc0a98c4eca
rubens-lavor <haryson07@gmail.com>
2021-04-27 22:55:11 -0300

    generics

---
+++ src/04 - Generics/01 - intro. tipos genéricos/intro.ts
@@ -0,0 +1,17 @@
+type filterCallback<U> = (value: U, index?: number, array?: U[]) => boolean
+
+export function myFilter<T>(array: T[], callbackFn: filterCallback<T>): T[] {
+  const newArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackFn(array[i])) {
+      newArray.push(array[i])
+    }
+  }
+  return newArray
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a']
+
+const result = myFilter(array, (value) => value < 5)
+console.log(result)
