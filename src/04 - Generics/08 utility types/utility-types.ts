fd4c1dc7323442fbfda647f664d38ce14a165b62
rubens-lavor <haryson07@gmail.com>
2021-05-03 12:31:56 -0300

    utility-types: Readonly, Pick, Extract e Exclude

--- src/04 - Generics/08 utility types/utility-types.ts
+++ src/04 - Generics/08 utility types/utility-types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Record
 const objeto1: Record<string, string | number> = {
   nome: 'maria',
@@ -19,6 +20,13 @@ type PessoaRequired = Required<PessoaProtocol>
 // Partial
 type PessoaPartial = Partial<PessoaRequired>
 
+// Readonly
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type PessoaReadeonly = Readonly<PessoaRequired>
+
+// Pick
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>
+
 const objeto2: PessoaRequired = {
   nome: 'josé',
   sobrenome: 'silva',
@@ -33,6 +41,37 @@ const objeto3: PessoaPartial = {
 
 console.log(objeto2)
 console.log(objeto3)
+
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C'
+type CDE = 'C' | 'D' | 'E'
+
+type TipoExclude = Exclude<ABC, CDE>
+type TipoExtract = Extract<ABC, CDE>
+
+type AccountMongo = {
+  _id: string
+  nome: string
+  idade: number
+}
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string
+}
+
+const accountMongo: AccountMongo = {
+  _id: 'dwejoiedjiodedwepd',
+  nome: 'luiz',
+  idade: 30,
+}
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo
+  return { ...accountData, id: _id }
+}
+
+const accountApi = mapAccount(accountMongo)
+console.log(accountApi)
 
 // Module node
 export default 1
