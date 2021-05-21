9e417604ddeb7a307c8ee3461be89ac6965bf109
rubens-lavor <haryson07@gmail.com>
2021-05-20 22:23:59 -0300

    decoradores de métodos

---
+++ src/05 - Decorators/Decorator Method/decorator-method.ts
@@ -0,0 +1,47 @@
+function decorator(
+  classPrototype: any,
+  nomeMetodo: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype)
+  console.log(nomeMetodo)
+  console.log(descriptor)
+
+  return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase()
+    },
+  }
+}
+
+export class UmaPessoa {
+  nome: string
+  sobrenome: string
+  idade: number
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.idade = idade
+  }
+
+  @decorator
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g)
+    const primeiroNome = palavras.shift()
+    if (!primeiroNome) return
+    this.nome = primeiroNome
+    this.sobrenome = palavras.join(' ')
+  }
+}
+
+const pessoa = new UmaPessoa('josé', 'souza', 42)
+const metodo = pessoa.metodo('Olá mundo!')
+console.log(metodo)
